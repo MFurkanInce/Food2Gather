014f88e05a2efbd7e4a5a53517b5e96aef3912f6
M.Furkan İNCE <mfrknince@gmail.com>
2023-01-22 13:16:16 +0300

    added prices for memberships

--- Membership.py
+++ Membership.py
@@ -1,7 +1,13 @@
 class MembershipClass:
     def __init__(self):
         self.subscription_type = ['gold', 'platinum', 'diamond']
+        self.price = {'gold': 150,
+                      'platinum': 200,
+                      'diamond': 250}
 
     def buy_ship(self, option):
         if option in self.subscription_type:
+            print('Thank you for purchasing our membership plan')
+            print(f'Plan : f{option}'
+                  f'Price : f{self.price[option]}')
             return option
